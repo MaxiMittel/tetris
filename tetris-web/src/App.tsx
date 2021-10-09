a09a08a8aafc6b531ae1232e018cc2470e372e9c
Maximilian Mittelhammer <maxi.mittelhammer@tum.de>
2021-10-09 16:37:04 +0200

    Added routing

--- tetris-web/src/App.tsx
+++ tetris-web/src/App.tsx
@@ -3,6 +3,10 @@ import { Navbar } from "./components/Navbar";
 import "./index.css";
 import "halfmoon/css/halfmoon-variables.min.css";
 import { SignIn } from "./components/SignIn";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Lobbies } from "./components/Lobbies";
+import { SignUp } from "./components/SignUp";
+import { Account } from "./components/Account";
 
 function App() {
   const halfmoon = require("halfmoon");
@@ -14,7 +18,14 @@ function App() {
   return (
     <div className="App">
       <Navbar>
-        <SignIn></SignIn>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/account" component={Account} />
+            <Route path="/" component={Lobbies} />
+          </Switch>
+        </BrowserRouter>
       </Navbar>
     </div>
   );
