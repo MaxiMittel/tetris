d82ce9709c31518d0689e2adfbf96545f0343c83
Maximilian Mittelhammer <maxi.mittelhammer@tum.de>
2021-10-11 13:38:04 +0200

    Added socket connection

--- game-server/src/main.py
+++ game-server/src/main.py
@@ -1,41 +1,65 @@
-from flask import Flask, jsonify
-from flask_socketio import SocketIO, send, emit
+from flask import Flask, request
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
+from flask_cors import CORS
+import json
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'CHANGE_SECRET!'
-socketio = SocketIO(app)
+CORS(app)
 
-# database or in memory storage room + players
+app.config['SECRET_KEY'] = 'CHANGE_SECRET!'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+database = {} # {'room_name': {'players': [{'sid': string, 'username': string, 'id': string}], 'game': {'field': []}}}
 
 @socketio.on('join')
 def join(data):
-    # data = {"room": "roomCode", "username": "Player0", "id": "f8hsdunjsdf"}
-    # Save player to room
-    send(jsonify({
-        "action": "onJoin",
-        "players": [
-            {
-                "username": "Player1",
-                "id": "f8hsdunjsdf"
-            }]}), broadcast=True)
+    room_name = data['room']
+    username = data['username']
+    id = data['id']
+    sid = request.sid
+
+    join_room(room_name)
+
+    if room_name not in database:
+        database[room_name] = {'players': [], 'game': {'field': []}}
+    else:
+        # TODO: check if player is already in room
+        database[room_name]['players'].append({'sid': sid, 'username': username, 'id': id})
+
+    emit('onJoin', {"players": database[room_name]['players']}, room=room_name)
+        
 
 @socketio.on('leave')
 def leave(data):
-    # data = {"room": "roomCode", "username": "Player0", "id": "f8hsdunjsdf"}
-    # Save player to room
-    send(jsonify({
-        "action": "onLeave",
-        "players": [
-            {
-                "username": "Player0",
-                "id": "f8hsdunjsdf"
-            },
-            {
-                "username": "Player1",
-                "id": "f8hsdunjsdf"
-            }]}), broadcast=True)
+    room_name = data['room']
+    sid = request.sid
+
+    leave_room(room_name)
+
+    if room_name in database:
+        database[room_name]['players'] = [player for player in database[room_name]['players'] if player['sid'] != sid]
+
+    emit('onLeave', {"players": database[room_name]['players']}, room=room_name)
+
+@socketio.on('playerUpdate')
+def player_update(data):
+    room_name = data['room']
+    id = data['id']
+    block = data['block']
+
+    emit('onPlayerUpdate', {"id": id, "player": block}, room=room_name)
+
+@socketio.on('fieldUpdate')
+def field_update(data):
+    room_name = data['room']
+    field = data['field']
+
+    #TODO: check if row is complete
+
+    emit('onFieldUpdate', {"field": field}, room=room_name)
+
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
 
 
--- tetris-web/src/components/TetrisSocket.tsx
+++ tetris-web/src/components/TetrisSocket.tsx
@@ -1,24 +1,88 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { randomBlock } from "../tetris/blocks";
 import { Block, Colors } from "../types";
 import { Tetris } from "./Tetris";
+import io from "socket.io-client";
+
+const socket = io("ws://localhost:5000");
+const room = "room1";
+const username = "Maxi" + Math.floor(Math.random() * 100);
+const id = "1" + Math.floor(Math.random() * 100);
 
 interface Props {}
 
 export const TetrisSocket: React.FC<Props> = (props: Props) => {
-
   let fielda = new Array<Colors[]>(20);
 
   for (var i = 0; i < fielda.length; i++) {
     fielda[i] = new Array<Colors>(10).fill(Colors.EMPTY);
   }
 
+  type PlayerEntry = {
+    id: string;
+    username: string;
+    block: Block;
+  };
+
   const [player, setPlayer] = useState<Block>(randomBlock());
+  const [players, setPlayers] = useState<PlayerEntry[]>([]);
   const [field, setField] = useState(fielda);
+
+  /**
+   * Join the room
+   */
+  useEffect(() => {
+    socket.emit("join", { room: room, username: username, id: id });
+  }, [socket]);
+
+  /**
+   * React to socket events
+   */
+  useEffect(() => {
+    // New player joins
+    socket.on("onJoin", (response) => {
+      //Insert new player
+      response.players.forEach((player: any) => {
+        if (!players.some((p) => p.id === player.id)) {
+          player["block"] = undefined;
+          setPlayers((players) => [...players, player]);
+        }
+      });
+    });
+
+    // Player updates
+    socket.on("onPlayerUpdate", (response) => {
+      setPlayers((players) =>
+        players.map((p) => {
+          if (p.id === response.id) {
+            p.block = response.block;
+          }
+          return p;
+        })
+      );
+    });
+
+    // Field update
+    socket.on("onFieldUpdate", (response) => {      
+      setField(response.field);
+    });
+  }, [socket, players]);
+
+  /**
+   * Send any player update
+   */
+  useEffect(() => {
+    socket.emit("onPlayerUpdate", {
+      room: room,
+      id: id,
+      block: player,
+    });
+  }, [player]);
 
   const onBlockFix = (newField: Colors[][]) => {
     setField(newField);
     setPlayer(randomBlock());
+    socket.emit("fieldUpdate", { room: room, field: newField });
   };
 
   return (
